110ba003febadc44c6d556aefcddbb6a6bf5dd7b
Michael Mehler <mhmehler@wm.edu>
2025-09-18 14:31:22 -0400

    Add mimnimum working version

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+    return (
+    <div className="h-screen flex items-center justify-center">
+        <h1 className="text-3xl font-bold text-slate-800">
+            Hello, Netlify!
+        </h1>
+    </div>
+    );
+}
